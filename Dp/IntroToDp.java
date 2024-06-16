fd89f1580d7895b2213e0ed219a205c6255e30f0
GUPTA Ritik <ritik.gupta@soprabanking.com>
2024-06-16 16:15:25 +0530

    feat: adds fibonnacci series and intro to dp

---
+++ Dp/IntroToDp.java
@@ -0,0 +1,79 @@
+/**
+ * Recursion: Top Down approach
+ * 
+ * Time Complexity: O(2^n)
+ * Space Complexity: O(n) => Stack space
+ */
+
+class Solution {
+    public int fib(int n) {
+        if(n <= 1) return n;
+        return fib(n -1) + fib(n - 2);
+    }
+}
+
+/**
+ * Memoization: Top Down approach
+ * We tend to store the results of the subproblems to avoid redundant calculations.
+ * We store them inside Map/Table.
+ * 
+ * Fibnocci Series: Use 1D array to store the results of the subproblems.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n) + O(n) = O(n) => Stack space + Table space
+ */
+class Solution {
+    private int fibHelper(int n, int[] dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fibHelper(n - 1, dp) + fibHelper(n - 2, dp);
+    }
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        for(int i = 0; i <= n; i++)
+            dp[i] = -1;
+        return fibHelper(n, dp);
+    }
+}
+
+/**
+ * Tabulation: Bottom Up approach
+ * 
+ * Time complexity: O(n)
+ * Space Complexity: O(n) => Table space
+ */
+class Solution {
+    public int fib(int n) {
+        int res = 0;
+        if(n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+            dp[i] = -1;
+        for(int i = 2; i <= n; i++){
+            res = dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return res;
+    }
+}
+
+/**
+ * Space Optimized Tabulation: Bottom Up approach
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public int fib(int n) {
+        int res = 0;
+        if(n <= 1) return n;
+        int prev2 = 0;
+        int prev = 1;
+        for(int i = 2; i <= n; i++){
+            res = prev + prev2;
+            prev2 = prev;
+            prev = res;
+        }
+        return res;
+    }
+}
