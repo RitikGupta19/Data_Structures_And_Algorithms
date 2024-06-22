f6df97d7a26f386ce748e129549b7ff7f3562a12
GUPTA Ritik <ritik.gupta@soprabanking.com>
2024-06-22 21:35:13 +0530

    feat: adds strength of pass question

---
+++ Arrays/StrengthOfPass.java
@@ -0,0 +1,37 @@
+/**
+ * StrengthOfPass: Given a string, find the strength of the password.
+ * The strength of the password increases by one for one consonant and one vowel
+ * Eg: RitikGupta -> Ri ti Gu ta -> 4
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+import java.util.*;
+
+public class Main {
+    public static int findStrongPass(String str){
+      boolean consonant = false, vowel = false;
+      int count = 0;
+      char[] a = str.toCharArray();
+      for(int i = 0; i < a.length; i++){
+        if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u'){
+          vowel = true;
+        }
+        else {
+          consonant = true;
+        }
+        if(vowel && consonant){
+          count++;
+          vowel = false;
+          consonant = false;
+        }
+      }
+      return count;
+    }
+    
+    public static void main(String[] args) {
+      String ques = "thisisbeautiful";
+      int ans = findStrongPass(ques);
+      System.out.println("Ans: " + ans);
+  }
+}
