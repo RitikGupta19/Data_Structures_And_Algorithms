ecd9c4b24d1ee2e0e14b150fe55714913559ebfe
GUPTA Ritik <ritik.gupta@soprabanking.com>
2025-04-30 00:59:52 +0530

    feat: 4 sum problem

---
+++ Arrays/4Sum.java
@@ -0,0 +1,108 @@
+/**
+ * 4 Sum problem: Find the unique quadruplets in the array which gives the sum of target.
+ * Problem link: https://leetcode.com/problems/4sum/
+ * 
+ * The problem can be solved using three approaches:
+ * 1. Brute Force: O(N^4) time complexity and O(1) space complexity.
+ * 2. Hashing: O(N^3) time complexity and O(N) space complexity. (remainder technique)
+ * 3. Two Pointer: O(N^3) time complexity and O(1) space complexity.
+ */
+public class 4Sum {
+    // APPROACH 1
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                for(int k = j + 1; k < n; k++){
+                    if(k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    for(int l = k + 1; l < n; l++){
+                        if(l > k + 1 && nums[l] == nums[l - 1]) continue;
+                        long sum = (long)nums[i];
+                        sum += (long) nums[j];
+                        sum += (long) nums[k];
+                        sum += (long) nums[l];
+                        if(sum == target){
+                            List<Integer> temp = new ArrayList<>();
+                            temp.add(nums[i]);
+                            temp.add(nums[j]);
+                            temp.add(nums[k]);
+                            temp.add(nums[l]);  
+                            ans.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // APPROACH 2
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                Set<Long> hashSet = new HashSet<>();
+                for(int k = j + 1; k < n; k++){
+                    long sum = (long)nums[i];
+                    sum += (long) nums[j];
+                    sum += (long) nums[k];
+                    long rem = (long)target - sum;
+                    if(hashSet.contains(rem)){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add((int)rem);  
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                    hashSet.add((long)nums[k]);
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    // APPROACH 3
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while(k < l){
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[k] + (long)nums[l];
+                    if(sum == target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);  
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k - 1]) k++;
+                        while(k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                    else if(sum < target) k++;
+                    else l--;
+                }
+
+            }
+        }
+        return ans;
+    }
+}
